a3e4d8f7f6cd411f02f28f95b6c856458502bf13
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-07-16 14:32:19 +0300

    fear: adding origami components and styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,12 +5,11 @@ import styles from "./index.module.css";
 const Header = () => {
   return (
     <header className={styles.navigation}>
-      <ul>
-        <Link href="#" title="Going to 1" />
-        <Link href="#" title="Going to 2" />
-        <Link href="#" title="Going to 3" />
-        <Link href="#" title="Going to 4" />
-      </ul>
+      <Link href="#" title="Going to 1" type="header" />
+      <Link href="#" title="Going to 2" type="header" />
+      <Link href="#" title="Going to 3" type="header" />
+      <Link href="#" title="Going to 4" type="header" />
+      <Link href="#" title="Going to 5" type="header" />
     </header>
   );
 };
---
+++ src/components/origami/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Origami = ({ description, author }) => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.description}>{description}</p>
+      <div>
+        <span className={styles.user}>
+          <small>Author: </small>
+          {author.username}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Origami;
