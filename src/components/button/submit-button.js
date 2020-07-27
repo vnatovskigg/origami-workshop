8d5252bd1ec47948bc64342b07bc19bf934e5811
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-07-27 22:40:44 +0300

    feat: implementing login and register logic

--- src/components/button/submit-button.js
+++ src/components/button/submit-button.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./submit-button.module.css";
 
 const SubmitButton = ({ text }) => {
-  return <button className={styles.submit}>{text}</button>;
+  return (
+    <button type="submit" className={styles.submit}>
+      {text}
+    </button>
+  );
 };
 
 export default SubmitButton;
